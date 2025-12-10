d13ba3f7bfb208f6496dda358707d58953e60e44
Carlos Sevilla <carlos.sevilla@originpath.com>
2025-12-10 17:53:42 +0100

    Project initializr and extra initial dependencies

---
+++ similar-products-api/src/main/java/com/carlossevilla/similar_products_api/SimilarProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlossevilla.similar_products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductsApiApplication.class, args);
+	}
+
+}
---
+++ similar-products-api/src/test/java/com/carlossevilla/similar_products_api/SimilarProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlossevilla.similar_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
